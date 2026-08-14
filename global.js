0ded43b5097feab9f35e1dc5d20cf6a225babc40
quicksearchchinese <quicksearchchinese@gmail.com>
2026-08-14 00:51:43 -0400

    Update global.js

--- global.js
+++ global.js
@@ -2,5 +2,5 @@ document.getElementById("logo_div").addEventListener("click", () => {
     window.location.href = "https://quicksearchchinese.com";
 });
 document.getElementById("beta_div").addEventListener("click", () => {
-    window.open("./快查.apk");
+    window.open("https://drive.google.com/file/d/1VE71dMZce7-mu14zDAt0kZnzQM6lrYjW");
 });
