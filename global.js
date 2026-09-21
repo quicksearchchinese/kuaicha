92d7bdac4579c4a4b69609799ed3d8e10dab36a0
quicksearchchinese <quicksearchchinese@gmail.com>
2026-09-21 04:47:10 -0400

    Update global.js

--- global.js
+++ global.js
@@ -2,5 +2,5 @@ document.getElementById("logo_div").addEventListener("click", () => {
     window.location.href = "https://quicksearchchinese.com";
 });
 document.getElementById("beta_div").addEventListener("click", () => {
-    window.open("https://drive.google.com/file/d/1VnprB_cGA8odz9CKFqwIRHyrDezY9OL2");
+    window.open("https://drive.google.com/file/d/1hlM2zNPwZUcKaZW9FFxE7oARHlVwpQHe");
 });
